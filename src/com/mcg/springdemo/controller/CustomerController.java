91b3de4d7f91221e8c360722f71773330c3951e1
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-05-30 19:14:18 +0300

    Service layer added and refactoring RequestMapping to GetMapping.

--- src/com/mcg/springdemo/controller/CustomerController.java
+++ src/com/mcg/springdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.mcg.springdemo.controller;
 
 import com.mcg.springdemo.dao.CustomerDAO;
 import com.mcg.springdemo.entity.Customer;
+import com.mcg.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,13 +17,13 @@ import java.util.List;
 public class CustomerController {
 
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model){
 
-        // get customers from the dao
-        List<Customer> customerList = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> customerList = customerService.getCustomers();
 
         //add the customers to the model
         model.addAttribute("customers",customerList);
---
+++ src/com/mcg/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.mcg.springdemo.service;
+
+import com.mcg.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+}
